d62cd516cc89b8843874554c9f9865a705e3cbd2
irac <iracding@tvunetworks.com>
2020-12-29 10:29:02 +0800

    update

---
+++ GrpcAudioStreaming/GrpcAudioStreaming.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace GrpcAudioStreaming.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // When calling insecure gRPC services this switch must be set before creating the GrpcChannel/HttpClient.
+            // https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.0#call-insecure-grpc-services-with-net-core-client
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new AudioStream.AudioStreamClient(channel);
+            var format = client.GetFormat(new Empty());
+            var audioStream = client.GetStream(new Empty());
+
+            using var audioPlayer = new AudioPlayer(format.ToWaveFormat());
+            audioPlayer.Play();
+
+            await foreach (var sample in audioStream.ResponseStream.ReadAllAsync())
+            {
+                audioPlayer.AddSample(sample.Data.ToByteArray());
+            }
+        }
+    }
+}
---
+++ StreamingComputeAverage/server/Program.cs
@@ -0,0 +1,48 @@
+﻿using ComputeAverage;
+using Google.Protobuf.Reflection;
+using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = 
+                    { 
+                        ComputeAverageService.BindService(new ComputeAverageServiceImplementation()),
+                        ServerReflection.BindService(new ReflectionServiceImpl(new List<ServiceDescriptor>() { ComputeAverageService.Descriptor }))
+                    },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on the port: 50051");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Something went wrong - {e.Message}");
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
